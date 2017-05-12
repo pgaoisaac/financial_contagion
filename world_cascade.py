0372ec4b9658ed253da89fca1b102e1f9cfba09a
priyav <priyav@tigs-MacBook-Pro-2.local>
2017-05-12 07:31:55 -0400

    updated world cascade

--- world_cascade.py
+++ world_cascade.py
@@ -50,6 +50,8 @@ def run_claims_list(A, p_GDP, countries, theta):
         A: sensitivity matrix
         p_GDP: prices proportional to GDP
         countries: list of countries, same order as p_GDP
+        
+    Return: {countryname: ['countriesfailedt1,countriesfailedt1', 'countrysfailedt2,contriesfailedt2', '.']}
     """
     failures = {}
     for i in range(len(p_GDP)):
@@ -57,7 +59,7 @@ def run_claims_list(A, p_GDP, countries, theta):
         p = np.copy(p_GDP)
         p.itemset(i, np.matmul(D, p_GDP)[i]/2)
         
-        failure_list = run_cascade(A, D, p, theta, v_0=p_GDP)
+        failure_list = run_cascade(A, D, p, theta, v_0=np.matmul(A, p_GDP))
         
         # transform into countries
         countries_failed = map(lambda lst: ",".join([countries[i] for i in lst]), failure_list)
@@ -65,52 +67,124 @@ def run_claims_list(A, p_GDP, countries, theta):
         # resize each of the lists to make it a useable array
         max_timesteps = len(p_GDP)
 
-        countries_failed_array = np.concatenate([countries_failed] + np.tile(["."], (max_timesteps-len(countries_failed), 1)).tolist(), axis=0)
-                                                
-        #countries_failed_array = np.concatenate([np.concatenate([x, np.tile(".", max_timesteps-len(x))], axis=0)], axis=0)
+        countries_failed_array = np.concatenate([countries_failed] + np.tile([" "], (max_timesteps-len(countries_failed), 1)).tolist(), axis=0)                                        
         assert countries_failed_array.shape[0] == max_timesteps
         
-        #countries_failed_by_timestep = dict(zip(range(len(countries_failed)), countries_failed))
         failures[countries[i]] = countries_failed_array
     return failures
 
 
-def zip_two_failure_dicts(dict1, dict2):
-    """Assumed the two failure dicts from a constant theta
+
+# Vary theta and record size of cascade
+# Some countries like Greece had sensitive thresholds so intermediate amounts of failure not found
+def vary_theta_failures_by_country(A, p_GDP, countries):
+    """Runs the claims cascade by failing each country to half of v_0 in turn. 
     
-    Args:
-        dict1, dict2 of the form {countryname: countries_failed_array}}
-                                                
-    Returns:
-        dict {countryname: {0: [[countryfromdict1, countryfromdict2]
-                                [countryfromdict1, .]]}}"""
-    zipped = dict()
-    
-    for country in dict1.keys():
-        country_dict = np.concatenate([np.expand_dims(dict1[country], axis=1), np.expand_dims(dict2[country], axis=1)], axis=1)
-        zipped[country] = country_dict
+    Arguments:
+        A: Dependency matrix described in Elliott et al.
+        p_GDP: GDP for each country (in the same order as countries), normalized such that the LOWEST p_GDP == 1.0
+        countries: List of strings of country names.
         
-    return zipped
-
-def find_discrepancies(dict1, dict2):
-    """Given dicts from two analyses of cross holdings with the SAME countries, find discrepancies in failure order"""
-    for country in dict1.keys():
-        cd1 = dict1[country]
-        cd2 = dict2[country]
-        for i in range(len(cd1)):
-            if set(cd1[i].split(",")) != set(cd2[i].split(",")):
-                print "Discrepancy between models with %s failed at timestep %d: %s vs %s\n" % (country, i, cd1[i], cd2[i])
-
-theta_list = [.6, .9, .935, .95, .975]
-for theta in theta_list:
-    failures_9B = run_claims_list(A_9B, p_GDP, countries, theta)
-    failures_9D = run_claims_list(A_9D, p_GDP, countries, theta) 
-    zipped_failures = zip_two_failure_dicts(failures_9B, failures_9D)
+    Returns:
+        DataFrame with columns 'country', 'theta', 'failures' denoting the number of failures that country caused at varying values of theta.
+    """
     
-
-    failure_array = np.concatenate(map(lambda country: np.concatenate([[["Failed:", country]], 
-                                                       zipped_failures[country]], axis=0), zipped_failures.keys()), axis=1)
+    all_results = []
+    thetas = np.linspace(.7, 1.0, 100)
     
-    find_discrepancies(failures_9B, failures_9D)
-    outfile = "failures_%d.tsv" % int(theta*1000)
-    np.savetxt(outfile, failure_array, fmt="%s", delimiter="\t")
+    for i in thetas: 
+        failures_9D = run_claims_list(A, p_GDP, countries, i)
+        theta_results = []
+        
+        for j in range(len(countries)):
+            def count_failures(x):
+                c = x.split(",")
+                if c[0] != ' ':
+                    return len(c)
+                else:
+                    return 0
+    
+            country_failed = sum(map(count_failures, failures_9D[countries[j]]))
+            theta_results.append([countries[j], float(i), int(country_failed)])
+                           
+        all_results.append(theta_results)
+
+    tidy_df = np.asarray(np.concatenate(all_results, axis=0))                         
+    tidy_df = pd.DataFrame(tidy_df,
+                       columns=['country', 'theta', 'failures'])
+    
+    return tidy_df
+
+
+def plot_individual_country_cascades(tidy_df):
+    """Line plot for each country varying theta. 
+    
+    Arguments:
+        tidy_df: must have columns 'failures', 'theta', 'country'
+        
+    Plots to failures_vary_theta/{countryname}.png
+    """
+    
+    # plot each country individually
+    prefix = "failures_vary_theta/"
+    countries = list(set(tidy_df['country']))
+    for i in range(len(countries)):
+        country = countries[i]
+        values = map(float, tidy_df.groupby('country').get_group(country)['failures'])
+        thetas = map(float, tidy_df.groupby('country').get_group(country)['theta'])
+
+        seaborn.set_style("darkgrid")
+        seaborn.tsplot(values, time=thetas, err_style=None)
+        plt.title("Number of Failures Following %s Given Varying Theta" % country)
+        plt.ylabel("Total Number of Failures")
+        plt.xlabel("Theta")
+        
+        # save
+        outfile = prefix+"%s.png" % country
+        plt.savefig(outfile)
+        plt.close()
+        
+
+
+def plot_theta_first_failures(tidy_df):
+    """Barplot for theta to first failure for each country
+    
+    Arguments:
+        tidy_df: must have columns 'failures', 'theta', 'country' 
+        
+    Plots to first_failures_barplot.png"""
+    
+    tidy_df['failures'] = map(int, tidy_df['failures'])  
+    tidy_df['theta'] = map(float, tidy_df['theta'])
+    tidy_df['first_failure'] = map(lambda x: 1 if x==1 else 0, tidy_df['failures'])
+    
+    # now plot
+    NUM_COLORS = 4
+    cmap =  plt.cm.get_cmap(name='jet')
+    color = [cmap(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
+    
+    plt.figure(figsize=(20,15))
+    m = plot_df.sort(columns='theta', ascending=False).groupby('first_failure').get_group(1)
+    max_theta = plot_df.groupby('first_failure').get_group(1).groupby('country').agg({'theta': np.max})
+    max_theta.sort('theta', inplace=True)
+
+    # plot
+    ax = seaborn.barplot(x = max_theta.index, y = max_theta.theta, color = color[1], ci=None)
+
+    # tweak plot parameters
+    plt.ylim([.65, 1.0])
+    plt.ylabel("Theta", fontsize=28)
+    plt.xlabel(' ')
+    plt.title("Theta Threshold for First Failure", fontsize=36)
+    ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=55, fontsize=28)
+    ax.set_yticklabels(['0.65', '0.70', '0.75', '0.80', '0.85', '0.90', '0.95', '1.0'], fontsize=28)
+
+    # save
+    outfile = "first_failures_barplot.png"
+    plt.savefig(outfile)
+    plt.close()
+
+    
+failures_df = vary_theta_failures_by_country(A_9D, p_GDP, countries)   
+plot_individual_country_cascades(failures_df)
+plot_theta_first_failures(failures_df)
